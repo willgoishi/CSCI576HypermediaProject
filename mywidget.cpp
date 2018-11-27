05f6991b1c17dff320e57a7330ef4aa58629664d
Webber Wang <webber@codelabstudios.com>
2018-11-27 00:44:58 -0800

    Removed extra files

--- mywidget.cpp
+++
@@ -1,9 +0,0 @@
-#include "mywidget.h"
-#include <QWidget>
-
-mywidget::mywidget(QWidget *parent) : QWidget(parent)
-{
-    setAttribute(Qt::WA_StyledBackground, true);
-//    setAttribute(Qt::WA_TranslucentBackground, true);
-    this->setStyleSheet("background-color: rgba(0,0,0,0);");
-}
